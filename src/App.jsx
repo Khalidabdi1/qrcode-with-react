f9b73ae282b7c11d43e930a03705d0e8eab69339
Khalid Abdi <xamza2009@gmail.com>
2025-08-15 23:10:00 +0300

    fix alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
 import './App.css'
 import MakeQr from './com/MakeQr'
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from "lucide-react"
+import { Terminal } from "lucide-react";
+
 
 function App() {
 
   return (
     
-    <div className="bg-background text-foreground min-h-screen flex items-center justify-center" >
+    <div className="bg-background text-foreground min-h-screen flex items-center justify-center" style={{height:"100vh",width:"100vw"}}>
           
-          <MakeQr/>
-          
+         
+      <MakeQr />
+     
+    
+      
     </div>
     
   )
--- src/com/MakeQr.jsx
+++ src/com/MakeQr.jsx
@@ -1,5 +1,7 @@
 
-
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon } from "lucide-react"
+import { Terminal } from "lucide-react";
 
 import { Button } from "@/components/ui/button"
 import {
@@ -28,21 +30,43 @@ import { useState } from "react"
   let laba ;
 export default function MakeQr(){
 
-  let [selectInput,setSelectValue]=useState("email")
+  let [selectInput,setSelectValue]=useState({
+    name:"Email",
+    password:0,
+    protocals:"",
+    showAlert:false,
+  })
+  
 
 
-  if(selectInput ==="email"){
+  if(selectInput.name ==="Email"){
     laba="Email";
     holder="m@example.com";
-  }else if(selectInput ==="Url"){
+  }else if(selectInput.name ==="Url"){
         laba="Url";
     holder="https://example.com";
-  }else if(selectInput==="Phone number"){
+  }else if(selectInput.name==="Phone number"){
        laba="Phone number";
     holder="example : +201234567890";
-  }else if(selectInput==="SMS"){
+  }else if(selectInput.name==="SMS"){
     laba="SMS";
     holder="example : +201234567890";
+  }else if(selectInput.name==="Wi-Fi"){
+    laba="Wi-Fi Name";
+    holder="example";
+  }else if(selectInput.name==="Text"){
+    laba="write any Test";
+    holder=" hello world";
+  }
+
+
+  // create alert
+  function handleAlert(){
+    setSelectValue({...selectInput,showAlert:true})
+    setTimeout(()=>{
+    setSelectValue({...selectInput,showAlert:false})
+
+    },3000)
   }
     return(
       
@@ -60,7 +84,7 @@ export default function MakeQr(){
         <form>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
-              <Label htmlFor="email">{laba}</Label>
+              <Label htmlFor="email">{laba} </Label>
               <Input
                 id="email"
                 type="email"
@@ -72,17 +96,64 @@ export default function MakeQr(){
               />
             </div>
             <div className="grid gap-2">
-              <div className="flex items-center">
+              
+
+              {/** if user select wifi */}
+              {selectInput.name==="Wi-Fi"&& <div className="flex items-center">
+                <Label htmlFor="password">Protocals</Label>
+               
+              </div> }
+              
+             
+{selectInput.name==="Wi-Fi"&&
+
+<Select onValueChange={(value)=>{
+      console.log(value)
+      // setSelectValue({...selectInput,protocals:value})
+           setSelectValue({...selectInput,protocals:value})
+           
+      }} value={selectInput.protocals} >
+          <SelectTrigger className="w-full " >
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>type of protocals </SelectLabel>
+              
+               {/** WIFI:T:WPA;S:MyNetwork;P:mypassword;;*/}
+               {/** t = protocals , s =name of network , p = password*/}
+              <SelectItem value="WEP">WEP</SelectItem>
+               <SelectItem value="WPA">WPA</SelectItem>
+               <SelectItem value="Empty if without password">Empty if without password</SelectItem>
+
+
+            
+
+              
+
+
+            </SelectGroup>
+          </SelectContent>
+    </Select>
+
+
+}
+    
+
+{/**  end ====if user select wifi ====*/}
+
+
+<div className="flex items-center">
                 <Label htmlFor="password">Type</Label>
                
-              </div>
+ </div>
 
 {/** start of select */}
     <Select onValueChange={(value)=>{
       console.log(value)
-      setSelectValue(value)
+      setSelectValue({...selectInput,name:value})
            
-      }} value={selectInput}>
+      }} value={selectInput.name}>
           <SelectTrigger className="w-full " >
             <SelectValue placeholder="Select a fruit" />
           </SelectTrigger>
@@ -111,6 +182,9 @@ export default function MakeQr(){
     </Select>
 
 {/** end of select */}
+
+
+
              
 
             </div>
@@ -120,14 +194,34 @@ export default function MakeQr(){
       </CardContent>
 
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
+        <Button type="submit" className="w-full" onClick={(e)=>{
+          console.log("is click")
+          handleAlert()
+        }}>
          Create a qrcode
         </Button>
-        <Button variant="outline" className="w-full">
+        <Button variant="destructive" className="w-full">
          Download QR Code
         </Button>
       </CardFooter>
 
+
+
+{/** start of alert  */}
+{selectInput.showAlert===true &&
+
+  <Alert className="w-full max-w-sm rounded-xl border border-border bg-card text-card-foreground shadow-sm absolute left-0 bottom-0 m-5">
+        <AlertCircleIcon className="h-4 w-4 mr-2" />
+        <AlertTitle>qrcode made !</AlertTitle>
+        <AlertDescription>
+      You can download the qrcode now
+        </AlertDescription>
+      </Alert>
+}
+    
+
+
+      {/**  endstart of alert  */}
     </Card>
     )
 }
